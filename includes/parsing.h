978d8db6ceef689ef02731320c2360d2a0728d9f
ekhaled <ekhaled@student.42.fr>
2024-01-07 19:50:30 +0100

    [FIX] Unnecessary for the time being definition

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,14 +6,13 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 14:28:10 by ekhaled           #+#    #+#             */
-/*   Updated: 2024/01/03 19:17:18 by ekhaled          ###   ########.fr       */
+/*   Updated: 2024/01/06 22:01:11 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
 # define PARSING_H
 
-bool	run_repl(t_session *session);
 # include <stdint.h>
 # include <stdbool.h>
 
@@ -23,6 +22,7 @@ typedef struct s_lstr
 	uint32_t	len;
 }	t_lstr;
 
+// bool	run_repl(t_session *session);
 
 bool	is_word(t_lstr word_candidate);
 bool	is_name(t_lstr name_candidate);
