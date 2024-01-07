788bfbc7bdc010033b00896c2f6c9d4f44b5c807
ekhaled <ekhaled@student.42.fr>
2024-01-07 19:49:52 +0100

    [ADD] Env init now including default env creation + safe init order in case of early destroyer use

--- sources/session/init_session.c
+++ sources/session/init_session.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "session.h"
+#include "builtins.h"
 
 static bool	restore_hist(void)
 {
@@ -34,13 +35,54 @@ static bool	restore_hist(void)
 	return (1);
 }
 
+static bool	create_default_env(char ***env_p)
+{
+	char	*path;
+	char	*pwd_var;
+
+	path = getcwd(NULL, 0);
+	if (!path)
+		return (0);
+	pwd_var = ft_strjoin("PWD=", path);
+	free(path);
+	if (!pwd_var)
+		return (0);
+	if (export((char *[]){"export", pwd_var, NULL}, env_p))
+	{
+		free(pwd_var);
+		return (0);
+	}
+	free(pwd_var);
+	if (export((char *[]){"export", "SHLVL=1", NULL}, env_p))
+		return (0);
+	return (1);
+}
+
+static bool	init_env(char ***session_env_p, char **envp)
+{
+	if (!envp[0])
+	{
+		if (!create_default_env(session_env_p))
+			return (0);
+	}
+	else
+	{
+		*session_env_p = ft_stradup(envp);
+		if (!(*session_env_p))
+			return (0);
+	}
+	return (1);
+}
+
 bool	init_session(t_session *session, char **envp)
 {
-	if (!restore_hist())
-		return (0);
-	session->env = envp;
+	session->env = NULL;
 	session->history = NULL;
 	session->last_cmd_status = 0;
 	session->should_exit = false;
+	if (!(init_env(&session->env, envp)))
+		return (0);
+	if (!restore_hist())
+		return (0);
 	return (1);
 }
