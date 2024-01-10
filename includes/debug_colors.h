1ed9bb3406f763da27e4a6ea3d591d5836f64fe2
ekhaled <giangrandeelia@gmail.com>
2024-01-10 23:41:13 +0100

    [ADD] Token enum, cursor str struct, quotes struct, tokenizer needing defs

--- includes/debug_colors.h
+++ includes/debug_colors.h
@@ -1,24 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   debug_colors.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/27 20:51:57 by ekhaled           #+#    #+#             */
-/*   Updated: 2024/01/10 17:35:40 by ekhaled          ###   ########.fr       */
+/*   Created: 2023/07/12 12:45:34 by ekhaled           #+#    #+#             */
+/*   Updated: 2023/07/12 12:46:05 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef DEBUG_COLORS_H
+# define DEBUG_COLORS_H
 
-# include <unistd.h>
-# include <stdlib.h>
-
-# include "session.h"
-# include "parsing.h"
-
-# include "debug_colors.h"//debug
+# define KNRM "\x1B[0m"
+# define KRED "\x1B[31m"
+# define KGRN "\x1B[32m"
+# define KYEL "\x1B[33m"
+# define KBLU "\x1B[34m"
+# define KMAG "\x1B[35m"
+# define KCYN "\x1B[36m"
+# define KWHT "\x1B[37m"
+# define KEND "\033[0m"
 
 #endif
--- includes/parsing.h
+++ includes/parsing.h
@@ -6,7 +6,7 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 14:28:10 by ekhaled           #+#    #+#             */
-/*   Updated: 2024/01/08 21:38:10 by ekhaled          ###   ########.fr       */
+/*   Updated: 2024/01/10 17:24:44 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,37 @@
 # include <stdint.h>
 # include <stdbool.h>
 
+typedef struct s_session	t_session;
+
+/*-------- Tokenizer --------*/
+
+enum e_token_type
+{
+	DLESS_TOKEN,
+	DGREAT_TOKEN,
+	LESS_TOKEN,
+	GREAT_TOKEN,
+	WORD_TOKEN,
+	NEWLINE_TOKEN,
+	IO_NUMBER_TOKEN,
+	AND_IF_TOKEN,
+	OR_IF_TOKEN,
+	PIPE_TOKEN,
+	LPAR_TOKEN,
+	RPAR_TOKEN
+};
+
 typedef struct s_lstr
 {
 	char		*str;
 	uint32_t	len;
 }	t_lstr;
 
-// bool	run_repl(t_session *session);
+typedef struct s_cstr
+{
+	char		*str;
+	uint32_t	cursor;
+}	t_cstr;
 
 typedef struct s_token
 {
@@ -36,14 +60,25 @@ typedef struct s_token_queue
 	struct s_token_queue		*next;
 }	t_token_queue;
 
+typedef struct s_quotes
+{
+	bool	in;
+	char	type;
+}	t_quotes;
+
 t_token_queue	*ft_queuenew(t_token content);
 t_token_queue	*ft_queuelast(t_token_queue *queue);
 void			ft_queuepush(t_token_queue **queue, t_token_queue *new);
 t_token_queue	*ft_queuepop(t_token_queue **queue);
 void			ft_queuefree(t_token_queue *queue);
 
+bool			is_word(t_lstr word_candidate);
+bool			is_operator(t_lstr op_candidate);
+bool			is_name(t_lstr name_candidate);
 
-bool	is_word(t_lstr word_candidate);
-bool	is_name(t_lstr name_candidate);
+bool			generate_tokens(t_cstr *input, t_token_queue **token_queue);
+bool			delimit_token(t_cstr *input, t_lstr *token_lstr);
+
+bool			read_input(t_session *session, char **input_str);
 
 #endif
