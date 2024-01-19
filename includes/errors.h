ca15c4a90a09ddc4580c5466e8b5646b33379491
ekhaled <ekhaled@student.42.fr>
2024-01-19 20:38:57 +0100

    [ADD] disp_token_error def

--- includes/errors.h
+++ includes/errors.h
@@ -6,16 +6,19 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 22:19:27 by ekhaled           #+#    #+#             */
-/*   Updated: 2024/01/03 22:25:29 by ekhaled          ###   ########.fr       */
+/*   Updated: 2024/01/19 03:14:43 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERRORS_H
 # define ERRORS_H
 
+# include "parsing.h"
+
 # define SHOULD_PRINT_SHELL 1
 
 void	disp_error(bool should_print_minishell,
 			char *cmd, char *arg, char *error);
+void	disp_token_error(enum e_token_type token_type);
 
 #endif
