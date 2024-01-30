33ec86a17e61ca3d647f7b26e00479174cc06cbf
ekhaled <ekhaled@student.42.fr>
2024-01-30 07:32:50 +0100

    [RM] Redundant code body in is_word function

--- sources/parsing/tokenizer/token_recognition/is_word.c
+++ sources/parsing/tokenizer/token_recognition/is_word.c
@@ -6,7 +6,7 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 23:26:50 by ekhaled           #+#    #+#             */
-/*   Updated: 2024/01/09 22:40:55 by ekhaled          ###   ########.fr       */
+/*   Updated: 2024/01/15 23:43:47 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,5 @@
 
 bool	is_word(t_lstr word_candidate)
 {
-	if (word_candidate.len == 1
-		&& (word_candidate.str[0] == '|'
-			|| word_candidate.str[0] == '('
-			|| word_candidate.str[0] == ')'
-			|| word_candidate.str[0] == '<'
-			|| word_candidate.str[0] == '>'))
-		return (0);
-	if (word_candidate.len == 2
-		&& ((word_candidate.str[0] == '&' && word_candidate.str[1] == '&')
-			|| (word_candidate.str[0] == '|' && word_candidate.str[1] == '|')
-			|| (word_candidate.str[0] == '<' && word_candidate.str[1] == '<')
-			|| (word_candidate.str[0] == '>' && word_candidate.str[1] == '>')))
-		return (0);
-	return (1);
+	return (!is_operator(word_candidate));
 }
