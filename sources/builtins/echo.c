9a2ea48cca65a638aa548cc512f115a2aa0bc2ef
ekhaled <giangrandeelia@gmail.com>
2024-01-31 00:45:07 +0100

    [FIX] Obscure printf display issue

--- sources/builtins/echo.c
+++ sources/builtins/echo.c
@@ -6,14 +6,15 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 01:19:26 by ekhaled           #+#    #+#             */
-/*   Updated: 2024/01/28 12:40:51 by ekhaled          ###   ########.fr       */
+/*   Updated: 2024/01/31 00:43:08 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdbool.h>
 #include <stdio.h>
 
-#include "utils.h"
+#include "minishell.h"
+
 
 int	ft_echo(char **argv)
 {
@@ -26,12 +27,13 @@ int	ft_echo(char **argv)
 		i++;
 	if (argv[i])
 	{
-		printf("%s", argv[i]);
+		write(1, argv[i], ft_strlen(argv[i]));
 		i++;
 	}
 	while (argv[i])
 	{
-		printf(" %s", argv[i]);
+		write(1, " ", 1);
+		write(1, argv[i], ft_strlen(argv[i]));
 		i++;
 	}
 	if (should_output_newline)
