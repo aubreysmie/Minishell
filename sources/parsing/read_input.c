4fea29600e0b92b6769fa525f8d0fed5d53ae612
ekhaled <ekhaled@student.42.fr>
2024-01-30 09:27:20 +0100

    [ADD] ctrl-d management

--- sources/parsing/read_input.c
+++ sources/parsing/read_input.c
@@ -6,7 +6,7 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 14:37:58 by ekhaled           #+#    #+#             */
-/*   Updated: 2024/01/17 18:26:42 by ekhaled          ###   ########.fr       */
+/*   Updated: 2024/01/28 11:53:04 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,10 @@ bool	read_input(t_session *session, t_cstr *input)
 	while (!did_get_line)
 	{
 		input->str = readline("minishell$ ");
+		if (!input->str)
+			ft_exit((char *[]){"exit", NULL}, session);
 		if (input->str[0])
-		{
-			add_history(*input_str);
-			if (!ft_straadd(&session->history, *input_str))
-				return (0);
 			did_get_line = true;
-		}
 		else
 			free(input->str);
 	}
