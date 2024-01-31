0e113a83c182c278b0aa89a36742f407865b5a00
ekhaled <ekhaled@student.42.fr>
2024-01-31 12:04:45 +0100

    [ADD] Histfile adding after ctrl-D in main prompt

--- sources/parsing/read_input.c
+++ sources/parsing/read_input.c
@@ -6,7 +6,7 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 14:37:58 by ekhaled           #+#    #+#             */
-/*   Updated: 2024/01/28 11:53:04 by ekhaled          ###   ########.fr       */
+/*   Updated: 2024/01/31 11:34:30 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@ bool	read_input(t_session *session, t_cstr *input)
 	{
 		input->str = readline("minishell$ ");
 		if (!input->str)
+		{
+			add_history_to_histfile(session->history);
 			ft_exit((char *[]){"exit", NULL}, session);
+		}
 		if (input->str[0])
 			did_get_line = true;
 		else
