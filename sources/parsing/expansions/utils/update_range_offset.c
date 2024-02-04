00f9b6f039b25d1e641c3e169bd02a825abc2cb9
ekhaled <giangrandeelia@gmail.com>
2024-02-04 16:06:56 +0100

    [FIX] Invalid read on range updating

--- sources/parsing/expansions/utils/update_range_offset.c
+++ sources/parsing/expansions/utils/update_range_offset.c
@@ -6,7 +6,7 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 10:24:29 by ekhaled           #+#    #+#             */
-/*   Updated: 2024/02/02 10:31:04 by ekhaled          ###   ########.fr       */
+/*   Updated: 2024/02/04 16:05:09 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,15 @@ void	update_range_offset(t_range_array *range_array, int offset)
 
 	if (range_array->len == 0)
 		return ;
-	while (range_array->array[i].is_quoted)
-		i++;
-	range_array->array[i].end -= offset;
 	if (i == range_array->len - 1)
 	{
+		range_array->array[i].end -= offset;
 		i = 0;
 		return ;
 	}
+	while (range_array->array[i].is_quoted)
+		i++;
+	range_array->array[i].end -= offset;
 	j = i + 1;
 	while (j < range_array->len)
 	{
